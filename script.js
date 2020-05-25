fd5d57d96728f4df3865e57ec75a17ce1e562cd9
Nicholas Carrigan <nhcarrigan@gmail.com>
2020-05-24 17:53:19 -0700

    navbar adjusts margins

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function navMenu() {
   const nav = document.getElementById("navbar");
   const hamburger = document.getElementById("hamburger");
+  const main = document.getElementById("main");
   if (nav.style.display === "block") {
     nav.style.display = "none";
     hamburger.style.color = "#000000";
+    main.style.marginLeft = "0px";
   } else {
     nav.style.display = "block";
     hamburger.style.color = "#FFFFFF";
+    main.style.marginLeft = "200px";
   }
 }
