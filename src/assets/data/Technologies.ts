595cf0313435debf56ef909c4584cd0d9c3a8f8b
nhcarrigan <nhcarrigan@gmail.com>
2020-09-05 13:12:47 -0700

    Add icons

--- src/assets/data/Technologies.ts
+++ src/assets/data/Technologies.ts
@@ -9,3 +9,18 @@ export const Technologies = [
   'Python',
   'Angular',
 ];
+
+export const Icons: IconInt[] = [
+  {name: "HTML 5", icon: "fab fa-html5"},
+  {name: "CSS 3", icon: "fab fa-css3-alt"},
+  {name: "JavaScript", icon: "fab fa-js-square"},
+  {name: "node.js", icon: "fab fa-node"},
+  {name: "git", icon: "fab fa-git-alt"},
+  {name: "Python", icon: "fab fa-python"},
+  {name: "Angular", icon: "fab fa-angular"}
+]
+
+export interface IconInt {
+  name: string;
+  icon: string;
+}
