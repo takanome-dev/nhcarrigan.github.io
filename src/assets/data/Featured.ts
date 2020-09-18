e3f1ff53d361e1b957b11d5b874065b0740aa6a4
nhcarrigan <nhcarrigan@gmail.com>
2020-09-18 14:03:02 -0700

    New resume, favicon, featured project

--- src/assets/data/Featured.ts
+++ src/assets/data/Featured.ts
@@ -27,6 +27,15 @@ export const Featured: FeaturedInt[] = [
       'A messaage board allowing users to create posts and reply to topics. Still in alpha.',
   },
   {
+    title: 'Social Media Database',
+    image: 'social.png',
+    liveUrl: 'http://socialdb.ncarrigan.com',
+    codeUrl: 'https://github.com/nhcarrigan/social-db',
+    tech: ['TypeScript', 'node.js', 'Express', 'MongoDB + Mongoose'],
+    description:
+      "An app that allows users to create a virtual business card to share all of their social media profiles at one time.",
+  },
+  {
     title: 'Counting Cards',
     image: 'cards.jpg',
     liveUrl: 'https://www.nhcarrigan.com/counting-cards',
@@ -34,15 +43,6 @@ export const Featured: FeaturedInt[] = [
     tech: ['JavaScript', 'HTML', 'CSS'],
     description:
       'Offers three different levels of difficulty for learning how to count cards in Blackjack.',
-  },
-  {
-    title: 'Smash a Bug',
-    image: 'bug.jpg',
-    liveUrl: 'https://www.nhcarrigan.com/smash-a-bug',
-    codeUrl: 'https://github.com/nhcarrigan/counting-cards',
-    tech: ['JavaScript', 'HTML', 'CSS'],
-    description:
-      "A 'whack-a-mole' clone. Uses logic to determine when the player has successfully smashed a bug, tracks when the goal is met.",
   },
   {
     title: 'National Parks Search Page',
