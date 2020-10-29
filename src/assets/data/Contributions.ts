d128ed56484a74a7a3b9edcb86d7b10a3bc89532
nhcarrigan <nhcarrigan@gmail.com>
2020-10-29 09:29:29 -0700

    Update projects and contribs
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/assets/data/Contributions.ts
+++ src/assets/data/Contributions.ts
@@ -20,4 +20,14 @@ export const Contributions: ContribInt[] = [
     owner: 'Twaha Rahman',
     url: 'https://github.com/Twaha-Rahman/e-commercify',
   },
+  {
+    name: 'Hacktoberfest Checker',
+    owner: 'Luke Oliff',
+    url: 'https://github.com/lukeocodes/hacktoberfest-checker',
+  },
+  {
+    name: 'do_username',
+    owner: 'Matt Cowley',
+    url: 'https://github.com/MattIPv4/do_username',
+  },
 ];
