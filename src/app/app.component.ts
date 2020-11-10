3d062df8d2325753289626d152783efefd40ed67
nhcarrigan <nhcarrigan@gmail.com>
2020-11-10 11:24:02 -0800

    More straggler
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
-declare let gtag: Function;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,13 +8,5 @@ declare let gtag: Function;
 })
 export class AppComponent {
   title = 'Nicholas Carrigan';
-  constructor(public router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'UA-176870980-1', {
-          'page-path': event.urlAfterRedirects,
-        });
-      }
-    });
-  }
+  constructor(public router: Router) {}
 }
