6289bcb2a7580c618d42f6970a23a97b60fee7bb
nhcarrigan <nhcarrigan@gmail.com>
2021-01-03 10:59:18 -0800

    feat: Objectify sponsors
    Creates a .ts file to hold sponsor data (if I ever get any...) for quick
    and modular updating.
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SponsorsList } from "../../assets/data/Sponsors";
 
 @Component({
   selector: 'app-sponsors',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponsors.component.css']
 })
 export class SponsorsComponent implements OnInit {
+  platinum = [];
+  gold = [];
+  silver = [];
+  bronze = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.platinum = SponsorsList.platinum
+    this.gold = SponsorsList.gold;
+    this.silver = SponsorsList.silver;
+    this.bronze = SponsorsList.bronze;
   }
 
 }
