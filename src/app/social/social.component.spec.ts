bfa65554cfb89c64cea8acece0ab4140bfc27efd
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-09 16:27:42 -0700

    feat(tools): social tests
    Add tests for the social component.

--- src/app/social/social.component.spec.ts
+++ src/app/social/social.component.spec.ts
@@ -1,25 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { social } from 'src/assets/data/Social';
 
 import { SocialComponent } from './social.component';
 
 describe('SocialComponent', () => {
   let component: SocialComponent;
   let fixture: ComponentFixture<SocialComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [SocialComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(SocialComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render', () => {
+    expect(compiled).toBeTruthy();
+  });
+
+  it('should have the correct properties', () => {
+    expect(component.socials).toBeDefined();
+    expect(component.socials).toBe(social);
+  });
+
+  it('should render the correct title', () => {
+    const title = compiled.querySelector('h1');
+    expect(title).toBeTruthy();
+    expect(title.innerText.trim()).toBe('My Socials');
+  });
+
+  social.forEach((item, index) => {
+    it(`should render the ${item.site} data correctly`, () => {
+      const icon = compiled.querySelectorAll('i')[index];
+      const parsedIcon = icon.className.split(' ').reverse().join(' ');
+      const description = compiled.querySelectorAll('.social-desc')[index];
+      expect(parsedIcon).toBe(item.icon);
+      expect(description.innerText.trim()).toBe(item.description);
+    });
   });
 });
