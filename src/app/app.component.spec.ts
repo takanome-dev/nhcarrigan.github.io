a120605d4f79e32d4d8dcc1c7c041b4f4064c48b
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-09 15:26:29 -0700

    feat(tools): root tests
    Tests for the root app component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,12 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, CharacterComponent, NavComponent, AudioComponent],
+      declarations: [
+        AppComponent,
+        CharacterComponent,
+        NavComponent,
+        AudioComponent,
+      ],
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
@@ -31,7 +36,15 @@ describe('AppComponent', () => {
     );
   });
 
-  it('should render body component', () => {
+  it('should render the character component', () => {
+    expect(compiled.querySelector('app-character')).toBeTruthy();
+  });
+
+  it('should render the navbar component', () => {
+    expect(compiled.querySelector('app-nav')).toBeTruthy();
+  });
+
+  it('should render the router component', () => {
     expect(compiled.querySelector('router-outlet')).toBeTruthy(
       'does not render router component'
     );
