486220789529b9a2311f9c9fabd162cb221d6a83
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-09 15:52:11 -0700

    feat(tools): contact tests
    Restore tests for the contact component.

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -5,21 +5,38 @@ import { ContactComponent } from './contact.component';
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ContactComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render', () => {
+    expect(compiled).toBeTruthy();
+  });
+
+  it('should render the title', () => {
+    expect(compiled.querySelector('h1').textContent).toBe('Contact me!');
+  });
+
+  it('should have correct form', () => {
+    const form = compiled.querySelector('form');
+    expect(form).toBeTruthy('does not render form');
+    expect(form.getAttribute('action')).toBe('https://formspree.io/f/xvowlnwe');
+    expect(form.getAttribute('method')).toBe('POST');
+  });
+
+  it('should render the calendar embed', () => {
+    expect(compiled.querySelector('.calendly-inline-widget')).toBeTruthy();
   });
 });
