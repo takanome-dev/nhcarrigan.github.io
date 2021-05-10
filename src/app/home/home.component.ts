168083bfcc45d0ada6836a94beaebba37475bfe2
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-09 17:49:06 -0700

    feat: display version
    Show the page version on the home view.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { links } from 'src/assets/data/Links';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   public links = links;
-  constructor() { }
+  public version = version;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
