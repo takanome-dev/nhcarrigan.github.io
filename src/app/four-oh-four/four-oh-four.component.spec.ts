8740e96719767bd72b30f67d798b956832e49156
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-10 22:56:31 -0700

    feat(tools): fourohfour tests
    Add tests for the FourOhFour component.

--- src/app/four-oh-four/four-oh-four.component.spec.ts
+++ src/app/four-oh-four/four-oh-four.component.spec.ts
@@ -5,21 +5,33 @@ import { FourOhFourComponent } from './four-oh-four.component';
 describe('FourOhFourComponent', () => {
   let component: FourOhFourComponent;
   let fixture: ComponentFixture<FourOhFourComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FourOhFourComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [FourOhFourComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(FourOhFourComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render', () => {
+    expect(compiled).toBeTruthy();
+  });
+
+  it('should have the expected links', () => {
+    const [github, status, chat] = compiled.querySelectorAll('a');
+    expect(github.innerText.trim()).toBe('GitHub');
+    expect(github.getAttribute('href')).toBe('https://github.com/nhcarrigan');
+    expect(status.innerText.trim()).toBe('Status Page');
+    expect(status.getAttribute('href')).toBe('http://status.nhcarrigan.com');
+    expect(chat.innerText.trim()).toBe('Chat Server');
+    expect(chat.getAttribute('href')).toBe('http://chat.nhcarrigan.com');
   });
 });
