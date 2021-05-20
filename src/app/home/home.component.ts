412790766d5737d5246bf45ad6ec5d55bc759fb5
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-20 06:13:50 -0700

    fix: manual package version
    Can revert this when Angular 12 allows default imports again.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { links } from 'src/assets/data/Links';
-import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +8,7 @@ import { version } from '../../../package.json';
 })
 export class HomeComponent implements OnInit {
   public links = links;
-  public version = version;
+  public version = '2.2.2';
   constructor() {}
 
   ngOnInit(): void {}
