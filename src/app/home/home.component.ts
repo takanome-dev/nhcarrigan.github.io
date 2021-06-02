86e990cd57034a1666705b4b56e6ee1d944522cb
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-06-01 23:21:51 -0700

    fix: tweak verbiage
    Slight rewording.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { links } from 'src/assets/data/Links';
 })
 export class HomeComponent implements OnInit {
   public links = links;
-  public years = Math.round(
+  public years = Math.floor(
     (Date.now() - new Date('April 15, 2020').getTime()) / 31557600000
   );
   public yearString = `${this.years} ${this.years === 1 ? 'year' : 'years'}`;
